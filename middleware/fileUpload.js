a307a9cc187dbe7f8ba8964f9d6d99cbacd4ac0f
princekumar2022 <pk279183@gmail.com>
2023-10-30 19:41:50 +0530

    30 th oct

--- middleware/fileUpload.js
+++ middleware/fileUpload.js
@@ -1,24 +1,48 @@
-const multer = require("multer");
-const path = require("path");
+// const multer = require("multer");
+// const path = require("path");
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads");
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(
-      null,
-      file.originalname.replace(" ", "-") +
-        "-" +
-        uniqueSuffix +
-        path.extname(file.originalname)
-    );
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "uploads");
+//   },
+//   filename: (req, file, cb) => {
+//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+//     cb(
+//       null,
+//       file.originalname.replace(" ", "-") +
+//         "-" +
+//         uniqueSuffix +
+//         path.extname(file.originalname)
+//     );
+//   },
+// });
+
+
+
+// const upload = multer({ storage: storage });
+
+// module.exports = upload;
+
+
+
+
+
+const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("cloudinary").v2;
+cloudinary.config({
+  cloud_name: 'https-www-pilkhuwahandloom-com',
+  api_key: '886273344769554',
+  api_secret: 'BVicyMGE04PrE7vWSorJ5txKmPs'
+});
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "images/image",
+    allowed_formats: ["jpg", "jpeg", "png", "PNG", "xlsx", "xls", "pdf", "PDF"],
   },
 });
-
-
-
 const upload = multer({ storage: storage });
+
 
 module.exports = upload;
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(compression({ threshold: 500 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use('/uploads', express.static(path.join(__dirname, './uploads')))
 if (process.env.NODE_ENV == "production") {
   console.log = function () { };
 }
